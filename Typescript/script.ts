76f0026ea0775773c7907ab1c31cbe14080ceea2
zalandooo <leander.cramer@hs-furtwangen.de>
2019-05-31 09:42:47 +0200

    steckbrrrrr

--- Typescript/script.ts
+++ Typescript/script.ts
@@ -67,5 +67,34 @@ document.getElementById("label").addEventListener("mouseover", hoverlabel)
 }
 
 
+document.getElementById("target-id").innerHTML
 
+var number = 2;
+viarable1 = Math.random (variable1)
+
+
+let monster1 : string = "asdf";
+let monster2 : string = "jklö";
+
+let monsterHitPoints0 = number 1;
+let monsterhitpoints1 = number 10;
+
+var monsterHitPoints = [5, 20];
+
+let monstername1 : string[];
+var monstername[0]="asdf";
+var monstername[1]="jklö";
+
+var playername = "Spielername";
+var suffix
+
+var monstername = ["asdf","jklö"];
+
+function getMonsterData ();
+
+[("Monster 0: " + monstername[0]+ " + monsterHitPoints[0])]
+
+Array > Schublade mit verschiedenen NAmen (Feld)
+eckige klammer = Array > var name: string [] = ["Wertzuweisung"]
+name.length > Länge eines Arrays
 
